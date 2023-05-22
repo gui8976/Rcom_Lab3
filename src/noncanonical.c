eef1e60607283e684677ca853554b1634d31fea5
gui8976 <76403371+gui8976@users.noreply.github.com>
2023-05-22 08:57:48 +0100

    Update noncanonical.c

--- src/noncanonical.c
+++ src/noncanonical.c
@@ -1,23 +1,17 @@
 /*Non-Canonical Input Processing*/
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <termios.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
 
+#include "variaveis.h"
 
-#define BAUDRATE B38400
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-
-volatile int STOP = FALSE;
-
-typedef enum
-{
+typedef enum {
     START,
     FLAG_RCV,
     A_RCV,
@@ -28,99 +22,82 @@ typedef enum
 
 message_state state = START;
 
-void state_handler(unsigned char c)
-{
+void state_handler(unsigned char c) {
     char buf[3];
-    switch (state)
-    {
+    switch (state) {
+        case START:
+            if (c == F)
+                state = FLAG_RCV;
+            else {
+                state = START;
+                break;
+            }
+        case FLAG_RCV:
+            if (c == A1 || c == A2) {
+                state = A_RCV;
+                buf[0] = c;
+                break;
+            } else if (c == F) {
+                state = FLAG_RCV;
+                break;
+            } else {
+                state = START;
+                break;
+            }
+        case A_RCV:
+            if (c == C_SET) {
+                buf[1] = c;
+                state = C_RCV;
+                break;
+            } else if (c == F) {
+                state = FLAG_RCV;
+                break;
+            } else {
+                state = START;
+                break;
+            }
 
-    case START:
-        if (c == 0x5C)
-            state = FLAG_RCV;
-        else
-        {
-            state = START;
-            break;
-        }
-    case FLAG_RCV:
-        if (c == 0x01 || c == 0x03)
-        {
-            state = A_RCV;
-            buf[0] = c;
-            break;
-        }
-        else if (c == 0x5C)
-        {
-            state = FLAG_RCV;
-            break;
-        }
-        else
-        {         
-            state = START;
-            break;
-        }
-    case A_RCV:
-        if (c == 0x03)
-        {
-            buf[1] = c;
-            state = C_RCV;
-            break;
-        }
-        else if (c == 0x5C)
-        {
-            state = FLAG_RCV;
-            break;
-        }
-        else
-        {
-            state = START;
-            break;
-        }
+        case C_RCV:
+            if (buf[0] ^ buf[1]) {
+                state = BCC_OK;
+                break;
+            } else if (c == F) {
+                state = FLAG_RCV;
+                break;
+            } else {
+                state = START;
+                break;
+            }
 
-    case C_RCV:
-        if (buf[0] ^ buf[1])
-        {
-            state = BCC_OK;
-            break;
-        }
-        else if (c == 0x5C)
-        {
-            state = FLAG_RCV;
-            break;
-        }
-        else
-        {
-            state = START;
-            break;
-        }
+        case BCC_OK:
+            if (c == F) {
+                state = STOP_a;
+                break;
+            } else {
+                state = START;
+                break;
+            }
 
-    case BCC_OK:
-        if (c == 0x5C)
-        {
-            state = STOP_a;
+        case STOP_a:
             break;
-        }
-        else
-        {
-            state = START;
-            break;
-        }
-
-    case STOP_a:
-        break;
     }
 }
 
-int main(int argc, char **argv)
-{
-    int fd, c, res, i = 0;
+int Set_machine(message_state state, int fd) {
+    if (state == STOP_a) {
+        printf("Set state achieved!\n");
+        char str[] = UA_1;
+        write(fd, str, sizeof str);
+        return 1;
+    }
+    return 0;
+}
+int main(int argc, char **argv) {
+    int fd, bytes_read = 0;
     struct termios oldtio, newtio;
-    char buff[255];
+    char buff;
 
-    if ((argc < 2) ||
-        ((strcmp("/dev/ttyS10", argv[1]) != 0) &&
-         (strcmp("/dev/ttyS11", argv[1]) != 0)))
-    {
+    if (argc < 2) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
@@ -131,14 +108,12 @@ int main(int argc, char **argv)
     */
 
     fd = open(argv[1], O_RDWR | O_NOCTTY);
-    if (fd < 0)
-    {
+    if (fd < 0) {
         perror(argv[1]);
         exit(-1);
     }
 
-    if (tcgetattr(fd, &oldtio) == -1)
-    { /* save current port settings */
+    if (tcgetattr(fd, &oldtio) == -1) { /* save current port settings */
         perror("tcgetattr");
         exit(-1);
     }
@@ -150,38 +125,26 @@ int main(int argc, char **argv)
 
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
-    newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
-    newtio.c_cc[VMIN] = 1;  /* blocking read until 5 chars received */
-
-    /*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) próximo(s) caracter(es)
-    */
+    newtio.c_cc[VTIME] = 1; /* inter-character timer unused */
+    newtio.c_cc[VMIN] = 0;
 
     tcflush(fd, TCIOFLUSH);
 
-    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-    {
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
         perror("tcsetattr");
         exit(-1);
     }
 
     printf("New termios structure set\n");
-    printf("New termios structure set\n");
-    while (STOP == FALSE)
-    {                           /* loop for input */
-        res = read(fd, buff, 1); /* returns after 5 chars have been input */
-        state_handler(buff[0]);
-        if (state == STOP_a)
-        {
-            printf("Set state achieved!\n");
-            char str[] = {0x5C, 0x03, 0x07, 0x06, 0x5C};
-            str[3] = str[1]^str[2];
-            write(fd, str, 255);
+    while (true) {
+        bytes_read = read(fd, &buff, sizeof(buff));
+
+        if (!bytes_read) break;
+
+        state_handler(buff);
+        if (Set_machine(state, fd))
             break;
-        }
     }
-    // printf("writing Back: %s", str);
     sleep(1);
     tcsetattr(fd, TCSANOW, &oldtio);
     close(fd);
